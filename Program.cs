064b5c297333e61ae5d72abcacd49634e11ecabd
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-10-07 07:17:23 -0300

    Resolução do Exercício

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            double peso;
+            double altura;
+
+            Console.Write("Altura (m)..: ");
+            altura = double.Parse(Console.ReadLine());
+
+            Console.Write("Peso (kg)...: ");
+            peso = double.Parse(Console.ReadLine());
+
+            double alturares;
+            double resultadofinal;
+
+            alturares = (altura * 2);
+            resultadofinal = peso / alturares;
+
+            Console.WriteLine();
+
+            Console.WriteLine($"IMC: {resultadofinal:N1} kg/m²");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Presione qualquer tecla para finalizar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
